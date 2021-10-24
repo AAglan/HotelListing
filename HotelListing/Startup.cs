dd9837b5b0e5fe7333cf57a578964227ccb8e90b
MOHAMED AGOUR <mmedhat@bebrand.tv>
2021-10-24 11:12:42 +0200

    Add DataBase

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -1,7 +1,9 @@
+using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +28,10 @@ namespace HotelListing
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddDbContext<DataBaseContext>(
+                options => 
+                options.UseSqlServer(Configuration.GetConnectionString("SqlConnection"))
+                );
 
             //use Cors orgin policy>>>>>>>>>>>>>>>>>>>>>>>>>
             services.AddCors(o =>
@@ -43,6 +47,9 @@ namespace HotelListing
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
             });
+
+            services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
